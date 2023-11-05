052ac019efa823505990d83bd259cb308fcb4481
DijoG <dijogergo@gmail.com>
2023-11-05 12:52:36 +0100

    parameter return = TRUE or FALSE added to get_3DTREE()

--- R/treecbh.R
+++ R/treecbh.R
@@ -10,9 +10,10 @@ stop_noerr <- function() {
 #' @param normalize logical, normalization based on CSF-classified ground points using the lidR::knnidw(), if FALSE "flat normalization" is performed
 #' @param output_dir string, path to output directory
 #' @param FEATURE character, attribute name and it values (can only be numeric!) to extract from multiPOLY to store in las header
+#' @param return logical, whether to return the list of las or not (default = TRUE)
 #' @return list of las files (point clouds of individual tree segments)
 #' @export
-get_3DTREE <- function(lasFILE, multiPOLY, normalize = TRUE, output_dir, FEATURE = NULL) {
+get_3DTREE <- function(lasFILE, multiPOLY, normalize = TRUE, output_dir, FEATURE = NULL, return = TRUE) {
 
   llas <- list()
   lground <- list()
@@ -55,7 +56,9 @@ get_3DTREE <- function(lasFILE, multiPOLY, normalize = TRUE, output_dir, FEATURE
     setwd(output_dir)
     writeLAS(llas[[i]], str_c("tree_0", i, ".las"))
   }
-  return(llas)
+  if (return) {
+    return(llas)
+  }
 }
 
 #' Function for executing CloudCompare plug-in, used in get_SEG(), taken from:

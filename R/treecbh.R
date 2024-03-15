5c7c1b530111255cd07b55378365cc2d6eee002c
DijoG <dijogergo@gmail.com>
2024-03-15 15:30:12 +0100

    ready

--- R/treecbh.R
+++ R/treecbh.R
@@ -481,6 +481,11 @@ get_CANOPYBH <- function(hist_DAT) {
     stop(return(height = gpbdf$y %>% max + 1))
   }
 
+  #> No kernel >
+  if (length(n_n) == 0 | class(n_n) != "numeric") {
+    stop(return(height = 0) )
+  }
+
   #> One kernel >
   if (length(n_n) == 1) {
     z_r =
